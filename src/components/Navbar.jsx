38de0f70193372a28428c1116261fb6b87d8ccbb
Franchuteelmastop <francoramospaypal@gmail.com>
2025-10-09 17:33:54 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,12 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <div className="nav-container">
-        <div className="nav-brand">TP4 - Lenguaje IV</div>
+        <div className="nav-brand">TP5 - Lenguaje IV</div>
         <div className="nav-links">
           <Link to="/">Inicio</Link>
           <Link to="/servicios">Servicios</Link>
           <Link to="/contacto">Contacto</Link>
+          <Link to="/api">API</Link>
         </div>
       </div>
     </nav>
